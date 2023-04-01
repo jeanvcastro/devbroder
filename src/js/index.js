8fac1cdad7f07a06425df79d9423d0a320680475
jeanvcastro <jean.castro1@outlook.com>
2023-04-01 01:12:32 -0300

    feat: dark mode

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 import "../sass/index.scss";
 
 window.onload = () => {
+  //set theme based in user preference
+  const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (prefersDark) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+
   const collapse = document.querySelector("#collapse");
   const toggleCollapseBtn = document.querySelector("#toggle-collapse");
 
