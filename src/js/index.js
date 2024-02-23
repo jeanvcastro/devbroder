b9d21f88cb80c19146d075c08110f3c4e2931b31
jeanvcastro <jean.castro1@outlook.com>
2024-02-23 04:40:03 -0300

    feat: add new brand

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,31 @@
-import feather from "feather-icons";
 import "../sass/index.scss";
 
-feather.replace();
+const userPrefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+const themeToggle = document.getElementById("theme-toggle");
+let darkMode = userPrefersDark;
 
-//set theme based in user preference
-const html = document.documentElement;
-const toggleTheme = document.querySelector("#toggle-theme");
+const applyTheme = dark => {
+  document.body.classList.toggle("dark-mode", dark);
 
-const lightMessage = "Mudar para tema claro";
-const darkMessage = "Mudar para tema escuro";
+  document.querySelectorAll(".dark").forEach(element => {
+    element.style.display = dark ? "block" : "none";
+  });
 
-const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-if (prefersDark) {
-  html.setAttribute("data-theme", "dark");
-  toggleTheme.title = lightMessage;
+  document.querySelectorAll(".light").forEach(element => {
+    element.style.display = dark ? "none" : "block";
+  });
+};
+
+themeToggle.addEventListener("click", () => {
+  darkMode = !darkMode;
+  applyTheme(darkMode);
+  localStorage.setItem("darkMode", darkMode);
+});
+
+if (localStorage.getItem("darkMode") !== null) {
+  darkMode = localStorage.getItem("darkMode") === "true";
 } else {
-  html.setAttribute("data-theme", "light");
-  toggleTheme.title = darkMessage;
+  localStorage.setItem("darkMode", darkMode);
 }
 
-toggleTheme.addEventListener("click", () => {
-  const currentTheme = html.getAttribute("data-theme");
-  const newTheme = currentTheme === "dark" ? "light" : "dark";
-  toggleTheme.title = newTheme === "dark" ? lightMessage : darkMessage;
-  html.setAttribute("data-theme", newTheme);
-});
-
-const collapse = document.querySelector("#collapse");
-const toggleCollapseBtn = document.querySelector("#toggle-collapse");
-
-toggleCollapseBtn.addEventListener("click", () => {
-  collapse.classList.toggle("show");
-});
-
-[...document.querySelectorAll("#collapse a")].forEach(a => {
-  a.addEventListener("click", () => {
-    collapse.classList.remove("show");
-  });
-});
+applyTheme(darkMode);
